f08fd9b9efda5d7c6061bf23b10bba8c6d33426d
sakaki <91630610+sakaki91@users.noreply.github.com>
2026-04-22 17:15:13 -0300

    Update 3.2-7
    This update included complex changes. What exactly? The installation script was completely refactored, without the pretty UI, and at the same time without hidden logs. Now, umu has an independent prefix that ONLY it can use, unlike before, where the wine prefix was copied to prefix_umu. This resolved several UI errors (C# from TeknoParrot) and because Proton UMU uses Wine 9 as a base, also having automatic installation of Mono 9 in the umu prefix, while the Wine prefix uses Mono 11. Both share the same TeknoParrot binaries folder, so the only thing that changes is the underlying runner, the settings, adjustments, and saved games in one instance remain in the other.

--- install.sh
+++ install.sh
@@ -1,79 +1,83 @@
 #!/bin/bash
 
-AWL_NAME="Arcade Wrapper Linux" && AWL_VERSION="3.2-6" && AWL_LOG="/dev/null"
-DEFAULT_COLOR="\033[0m" BOLD_COLOR="\033[1m" && DONE_LOG="\e[1;32mOK\033[0m" && NORMAL_LOG="\e[1;34m*\033[0m" && ERROR_LOG="\e[1;31mERROR\033[0m"
-TREE=${HOME}/TeknoParrot
+SCRIPT_VERSION="3.2-7"
 DXVK_VERSION="2.7.1"
 UMU_VERSION="10.0-4"
+UMU_MONO_VERSION="10.0.0"
+TREE=${HOME}/.local/share/awl
 
 primaryDependencyChecker(){
     dependencies=(umu-run wine bash wget unzip tar zenity)
     for cmd in "${dependencies[@]}"; do
         if ! command -v "$cmd" >/dev/null 2>&1; then
-            printf "$NORMAL_LOG $cmd not found. $ERROR_LOG\n" && exit 1
+            printf "$NORMAL_LOG $cmd not found.\n" && exit 1
         fi
     done
 }
 
 atomicTree(){
     PROGRAM=${TREE}/bin
+    TMP=${TREE}/tmp
     PREFIX=${TREE}/pfx
-    TMP=${PREFIX}/drive_c/tmp
+    PREFIX_UMU=${TREE}/pfx_umu
     [ -f "$TREE"/awl ] && rm -r "$TREE"/awl
-    [ -d "$PROGRAM" ] && rm -r "$PROGRAM"
-    [ -d "$PREFIX" ] && rm -r "$PREFIX" 
     [ -d "$TREE"/umu ] && rm -r umu
-    mv "$TREE" -T "$TREE".old
-    mkdir -p "$TREE"/{bin,pfx} && mkdir -p "$PREFIX/drive_c/tmp"
+    [ -d "$PROGRAM" ] && rm -r "$PROGRAM"
+    [ -d "$TMP" ] && rm -r "$TMP"
+    [ -d "$PREFIX" ] && rm -r "$PREFIX"
+    [ -d "$PREFIX_UMU" ] && rm -r "$PREFIX_UMU"
+    mkdir -p "$TREE"/{bin,pfx,pfx_umu,tmp}
 }
 
 dependencyInstall(){
-    clear
+#dependencies (download + extract) ==>
+    wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP"
+    wget -c https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe --directory-prefix="$TMP"
+    wget -c https://download.microsoft.com/download/8/4/a/84a35bf1-dafe-4ae8-82af-ad2ae20b6b14/directx_Jun2010_redist.exe --directory-prefix="$TMP"
+    wget -c https://github.com/doitsujin/dxvk/releases/download/v$DXVK_VERSION/dxvk-$DXVK_VERSION.tar.gz --directory-prefix="$TMP"
+    wget -c https://github.com/Open-Wine-Components/umu-proton/releases/download/UMU-Proton-$UMU_VERSION/UMU-Proton-$UMU_VERSION.tar.gz --directory-prefix="$TMP"
+    wget -c https://github.com/wine-mono/wine-mono/releases/download/wine-mono-$UMU_MONO_VERSION/wine-mono-$UMU_MONO_VERSION-x86.msi --directory-prefix="$TMP"
+    [ ! -f "$TMP"/dotnet-runtime-win-x64.exe ] && exit
+    [ ! -f "$TMP"/windowsdesktop-runtime-win-x64.exe ] && exit
+    [ ! -f "$TMP"/directx_Jun2010_redist.exe ] && exit
+    [ ! -f "$TMP"/dxvk-$DXVK_VERSION.tar.gz ] && exit
+    [ ! -f "$TMP"/wine-mono-$UMU_MONO_VERSION-x86.msi ] && exit
+    tar -xvf "$TMP"/UMU-Proton-$UMU_VERSION.tar.gz --directory "$TMP"
+    tar -xvf "$TMP"/dxvk-$DXVK_VERSION.tar.gz --directory "$TMP"
+    mv "$TMP"/UMU-Proton-$UMU_VERSION -T "$TREE"/umu
+#wine =>
     export WINEPREFIX=${PREFIX}
-    printf "$NORMAL_LOG Wineboot. ($PREFIX)" && wineboot -u &> $AWL_LOG
-    if [ -d "$PREFIX"/drive_c ]; then
-        printf " $DONE_LOG\n$NORMAL_LOG Downloading dependencies."
-        wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
-        wget -c https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
-        wget -c https://download.microsoft.com/download/8/4/a/84a35bf1-dafe-4ae8-82af-ad2ae20b6b14/directx_Jun2010_redist.exe --directory-prefix="$TMP" &>> $AWL_LOG
-        wget -c https://github.com/doitsujin/dxvk/releases/download/v$DXVK_VERSION/dxvk-$DXVK_VERSION.tar.gz --directory-prefix="$TMP" &>> $AWL_LOG
-        wget -c https://github.com/Open-Wine-Components/umu-proton/releases/download/UMU-Proton-$UMU_VERSION/UMU-Proton-$UMU_VERSION.tar.gz --directory-prefix="$TMP" &>> $AWL_LOG
-        [ ! -f "$TMP"/dotnet-runtime-win-x64.exe ] && printf " $ERROR_LOG\n" && exit
-        [ ! -f "$TMP"/windowsdesktop-runtime-win-x64.exe ] && printf " $ERROR_LOG\n" && exit
-        [ ! -f "$TMP"/directx_Jun2010_redist.exe ] && printf " $ERROR_LOG\n" && exit
-        [ ! -f "$TMP"/dxvk-$DXVK_VERSION.tar.gz ] && printf " $ERROR_LOG\n" && exit
-        printf " $DONE_LOG\n$NORMAL_LOG Installing dependencies."
-        wine "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart &>> $AWL_LOG
-        wine "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart &>> $AWL_LOG
-        wine "$TMP"/directx_Jun2010_redist.exe /Q /C /T:"C:\TMP" &>> $AWL_LOG
-        wine "$TMP"/DXSETUP.exe /silent &>> $AWL_LOG
-        tar -xvf "$TMP"/UMU-Proton-$UMU_VERSION.tar.gz --directory "$TMP" &>> $AWL_LOG
-        tar -xvf "$TMP"/dxvk-$DXVK_VERSION.tar.gz --directory "$TMP" &>> $AWL_LOG
-        mv "$TMP"/UMU-Proton-$UMU_VERSION -T "$TREE"/umu
-        mv "$TMP"/dxvk-$DXVK_VERSION/x32/*.dll "$PREFIX"/drive_c/windows/syswow64
-        mv "$TMP"/dxvk-$DXVK_VERSION/x64/*.dll "$PREFIX"/drive_c/windows/system32
-        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d10core /d native,builtin /f &>> $AWL_LOG
-        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d11 /d native,builtin /f &>> $AWL_LOG
-        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d8 /d native,builtin /f &>> $AWL_LOG
-        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d9 /d native,builtin /f &>> $AWL_LOG
-        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v dxgi /d native,builtin /f &>> $AWL_LOG
-        printf " $DONE_LOG\n$NORMAL_LOG Downloading TeknoParrot (Web-Installer)."
-        wget -c https://github.com/nzgamer41/TPBootstrapper/releases/latest/download/TPBootstrapper.zip --directory-prefix="$TMP" &>> $AWL_LOG
-        [ ! -f "$TMP"/TPBootstrapper.zip ] && printf " $ERROR_LOG\n" && exit
-        (
-        unzip "$TMP"/TPBootstrapper.zip -d "$PROGRAM" &>> $AWL_LOG
-        cd "$PROGRAM"
-        wine TPBootstrapper.exe &>> $AWL_LOG
-        [ -f "$PROGRAM"/TeknoParrotUi.exe ] && printf " $DONE_LOG\n"
-        [ ! -f "$PROGRAM"/TeknoParrotUi.exe ] && printf " $ERROR_LOG\n" && exit
-        )
-        ls $TREE &>> $AWL_LOG && ls $TREE/* &>> $AWL_LOG
-        rm -f "$PROGRAM"/TPBootstrapper*
-        rm -r "$TMP" && printf "$NORMAL_LOG Temporary files cleared. $DONE_LOG\n"
-        cp -rT "$PREFIX" "$TREE"/pfx_umu
-    else
-        printf " $ERROR_LOG\n" && exit
-    fi
+    wineboot -u
+    wine "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart
+    wine "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart
+    wine "$TMP"/directx_Jun2010_redist.exe /Q /C /T:"C:\tmp"
+    wine "$PREFIX"/drive_c/tmp/DXSETUP.exe /silent
+    mv "$TMP"/dxvk-$DXVK_VERSION/x32/*.dll "$PREFIX"/drive_c/windows/syswow64
+    mv "$TMP"/dxvk-$DXVK_VERSION/x64/*.dll "$PREFIX"/drive_c/windows/system32
+    wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d10core /d native,builtin /f
+    wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d11 /d native,builtin /f
+    wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d8 /d native,builtin /f
+    wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d9 /d native,builtin /f
+    wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v dxgi /d native,builtin /f
+#umu ==>
+    export GAMEID=0
+    export PROTONPATH="$TREE"/umu
+    export WINEPREFIX=${PREFIX_UMU}
+    umu-run wineboot -u
+    umu-run msiexec /i "$TMP"/wine-mono-$UMU_MONO_VERSION-x86.msi
+    umu-run "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart
+    umu-run "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart
+    umu-run "$TMP"/directx_Jun2010_redist.exe /Q /C /T:"C:\tmp"
+    umu-run "$PREFIX_UMU"/drive_c/tmp/DXSETUP.exe /silent
+#tekno ==>
+    (
+    wget -c https://github.com/nzgamer41/TPBootstrapper/releases/latest/download/TPBootstrapper.zip --directory-prefix="$TMP"
+    unzip "$TMP"/TPBootstrapper.zip -d "$PROGRAM"
+    cd "$PROGRAM"
+    wine TPBootstrapper.exe
+    rm -rf "$PROGRAM"/TPBootstrapper* "$TMP"
+    )
+rm -rf "$PREFIX"/drive_c/tmp "$PREFIX_UMU"/drive_c/tmp
 }
 
 executableCreation(){
@@ -83,23 +87,20 @@ executableCreation(){
     sed -i "s|^\s*UMU_LOCATION=.*|UMU_LOCATION=\"$TREE/umu\"|" awl.sh
     chmod +x awl.sh
     cp awl.sh "$TREE"/awl
+    ln -sf "$TREE"/awl "$HOME"/.local/bin/awl
 }
 
 case $1 in
     "--help")
-        echo -e "$AWL_NAME $AWL_VERSION\n$BOLD_COLOR--help$DEFAULT_COLOR\t\tShow this message.\n$BOLD_COLOR--version$DEFAULT_COLOR\tShow wrapper version.\n\n\e[0;40m./install @args$DEFAULT_COLOR\n\n@args:\n $BOLD_COLOR--debug$DEFAULT_COLOR\tThis executes the script and generates a log file (awl.log) in $HOME.\n $BOLD_COLOR--custom-dir$DEFAULT_COLOR\tWith this flag you can choose a custom installation directory."
+        echo -e "--help\t\tShow this message.\n--version\tShow wrapper version.\n--custom-dir\tWith this flag you can choose a custom installation directory."
+        exit
+    ;;
+    "--version")
+        printf "awl $SCRIPT_VERSION\n"
         exit
     ;;
     "--custom-dir")
-        TREE=$(zenity --file-selection --directory --title "Select your desired directory:")/TeknoParrot
-        [[ $2 == "-debug" ]] && AWL_LOG=$HOME/awl.log
-    ;;
-    "--debug")
-        AWL_LOG=$HOME/awl.log
-    ;;
-    "--version")
-        echo -e "$AWL_NAME $AWL_VERSION"
-        exit
+        TREE=$(zenity --file-selection --directory --title "Select your desired directory:")/awl
     ;;
 esac
 
